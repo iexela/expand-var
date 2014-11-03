931fc2c266e0fb1f2ae4a9acdee2cde90616f41d
Mitar <mitar.alex@gmail.com>
2014-11-03 16:47:01 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,156 @@
+
+var _ = require("underscore");
+
+var VAR_REG_EXP = /(\$\$)|(?:\$([a-zA-Z0-9_]+))|(?:\$\{([a-zA-Z0-9_\-]+)})/g;
+
+function isValue(obj) {
+  switch(typeof obj) {
+    case "string":
+    case "number":
+      return true;
+    case "object":
+      return _.isArray(obj);
+  }
+  return false;
+}
+
+function isContext(obj) {
+  return !isValue(obj);
+}
+
+function variables(str) {
+  var match,
+      vars = [];
+
+  while(true) {
+    match = VAR_REG_EXP.exec(str)
+    if(!match) {
+      break;
+    }
+
+    vars.push(match[2] || match[3]);
+  }
+
+  return vars;
+}
+
+function resolve(str, context) {
+  if(typeof str !== "string") {
+    return str;
+  }
+
+  return str.replace(VAR_REG_EXP, function(match, case0, case1, case2/*, case3, offset*/) {
+    if(case0) {
+      return "$";
+    }
+    else if(case1 && context[case1] != null) {
+      return context[case1];
+    }
+    else if(case2 && context[case2] != null) {
+      return context[case2];
+    }
+    //else if(case3) {
+    //  throw new TypeError("Illegal syntax of variable substitution near " + str.substring(offset, 10));
+    //}
+    else {
+      return match;
+    }
+  });
+}
+
+function chain(/* contexts */) {
+  return _.reduce(Array.prototype.slice.call(arguments).reverse(), function(l, r) {
+    var Clazz = function() {};
+    Clazz.prototype = l;
+    return _.extend(new Clazz(), r);
+  }, {});
+}
+
+function Stack() {
+  this.data = [];
+  this.calculating = {};
+}
+
+_.extend(Stack.prototype, {
+  push: function(n) {
+    if(this.calculating[n]) {
+      throw TypeError("Field '" + n + "' raises circular dependency");
+    }
+
+    this.data.push(n);
+    this.calculating[n] = true;
+  },
+  pop: function() {
+    var n = this.data.pop();
+    this.calculating[n] = false;
+    return n;
+  },
+  top: function() {
+    return this.data[this.data.length - 1];
+  },
+  empty: function() {
+    return this.data.length === 0;
+  }
+});
+
+function expandValue(root, contexts) {
+  var context = expandContext(variables(root), contexts);
+  return resolve(root, context);
+}
+
+function expandContext(keys, contexts) {
+  var raw = chain.apply(null, contexts),
+      cache = {},
+      hasInCache = _.bind(_.has, null, cache),
+      stack = new Stack(),
+      context = {};
+
+  _.each(keys, function(k) {
+    if(hasInCache(k)) {
+      context[k] = cache[k];
+      return;
+    }
+
+    stack.push(k);
+
+    while(!stack.empty()) {
+      var top = stack.top(),
+          vars = variables(raw[top]);
+
+      if(vars.length === 0) {
+        cache[top] = raw[top];
+        stack.pop();
+      }
+      else if(_.every(vars, hasInCache)) {
+        cache[top] = resolve(raw[top], cache);
+        stack.pop();
+      }
+      else {
+        _.each(vars, function(v) {
+          if(!hasInCache(v)) {
+            stack.push(v);
+          }
+        });
+      }
+    }
+
+    context[k] = cache[k];
+  });
+
+  return context;
+}
+
+var expand = function(root) {
+  if(arguments.length === 0) {
+    return undefined;
+  }
+
+  if(isValue(root)) {
+    return expandValue(root, _.rest(arguments));
+  }
+  else {
+    return expandContext(_.keys(root), arguments);
+  }
+};
+
+module.exports = expand;
---
+++ spec/expand-spec.js
@@ -0,0 +1,274 @@
+var expand = require("../index");
+
+describe("expand", function() {
+
+  var objectEquality = function(a, b) {
+    if(typeof a === "object" && typeof b === "object") {
+      var n;
+      for(n in a) {
+        if(a[n] !== b[n]) {
+          return false;
+        }
+      }
+
+      for(n in b) {
+        if(a[n] !== b[n]) {
+          return false;
+        }
+      }
+
+      return true;
+    }
+
+    return undefined;
+  };
+
+  beforeEach(function() {
+    jasmine.addCustomEqualityTester(objectEquality);
+  });
+
+  it("primitives", function() {
+    expect(expand("abc")).toBe("abc");
+    expect(expand(3)).toBe(3);
+  });
+
+  it("names", function() {
+    expect(expand("$abc", {
+      abc: 1
+    })).toBe("1");
+
+    expect(expand("$abc-def", {
+      "abc": 1,
+      "abc-": 2,
+      "abc-def": 3
+    })).toBe("1-def");
+
+    expect(expand("$abc-def", {
+      "abc-": 1,
+      "abc-def": 2
+    })).toBe("$abc-def");
+  });
+
+  it("string expansion", function() {
+    expect(expand("$a", {
+      a: 1
+    })).toBe("1");
+
+    expect(expand("${a}", {
+      a: 1
+    })).toBe("1");
+
+    expect(expand("$a $b", {
+      a: "$b",
+      b: 2
+    })).toBe("2 2");
+
+    expect(expand("$a ${c}", {
+      a: "$b",
+      b: 3,
+      c: 3
+    })).toBe("3 3");
+  });
+
+  it("string not found expansion", function() {
+    expect(expand("$b", {
+      b: ""
+    })).toBe("");
+
+    expect(expand("$b", {
+      b: null
+    })).toBe("$b");
+
+    expect(expand("$b", {
+      b: undefined
+    })).toBe("$b");
+  });
+
+  it("string expansion with nested context", function() {
+    expect(expand("$a", {
+      a: 1
+    }, {
+      a: 3
+    })).toBe("1");
+
+    expect(expand("$a", {
+      a: "$b",
+      b: 2
+    }, {
+      b: 3
+    })).toBe("2");
+
+    expect(expand("$a", {
+      a: "$b"
+    }, {
+      b: 3
+    }, {
+      b: 4
+    })).toBe("3");
+
+    expect(expand("$a", {
+      b: 5
+    }, {
+      a: "$b"
+    }, {
+      a: 1,
+      b: 2
+    })).toBe("5");
+  });
+
+  it("cycle expansion", function() {
+    expect(function() {
+      expand("$a", {
+        a: "$b",
+        b: "$a"
+      });
+    }).toThrow();
+
+    expect(function() {
+      expand({
+        a: "$b",
+        b: "$a"
+      });
+    }).toThrow();
+
+    expect(function() {
+      expand({
+        a: "$c",
+        b: "$a"
+      }, {
+        a: 1,
+        b: 3,
+        c: "$b"
+      });
+    }).toThrow();
+
+    expect(function() {
+      expand({
+        a: "$a"
+      }, {
+        a: 1
+      });
+    }).toThrow();
+
+    expect(function() {
+      expand({
+        a: "$b $c"
+      }, {
+        a: 1,
+        b: 3,
+        c: "$d"
+      }, {
+        d: "$a"
+      });
+    }).toThrow();
+  });
+
+  it("object expansion", function() {
+    expect(expand({
+      a: 1,
+      b: 2
+    }, {
+      c: 3,
+      d: 4
+    })).toEqual({
+      a: 1,
+      b: 2
+    });
+
+    expect(expand({
+      a: 1,
+      b: 2,
+      c: "$a"
+    })).toEqual({
+      a: 1,
+      b: 2,
+      c: "1"
+    });
+
+    expect(expand({
+      a: "$b $c $d",
+      b: 2
+    }, {
+      c: "1"
+    })).toEqual({
+      a: "2 1 $d",
+      b: 2
+    });
+  });
+
+  it("object expansion with nested context", function() {
+    expect(expand({
+      a: 1,
+      b: 2
+    }, {
+      b: 3,
+      a: 4,
+      c: 5
+    })).toEqual({
+      a: 1,
+      b: 2
+    });
+
+    expect(expand({
+      a: 1,
+      b: 2,
+      c: "$a"
+    })).toEqual({
+      a: 1,
+      b: 2,
+      c: "1"
+    });
+
+    expect(expand({
+      a: 1,
+      b: 2,
+      c: "$a"
+    })).toEqual({
+      a: 1,
+      b: 2,
+      c: "1"
+    });
+  });
+
+  it("special cases", function() {
+    expect(expand("$a$b", {
+      a: 1,
+      b: 2
+    })).toEqual("12");
+
+    expect(expand("${a}$b", {
+      a: 1,
+      b: 2
+    })).toEqual("12");
+
+    expect(expand("$", {
+      a: 1,
+      b: 2
+    })).toEqual("$");
+
+    expect(expand("${}", {
+      a: 1,
+      b: 2
+    })).toEqual("${}");
+
+    expect(expand("$$", {
+      a: 1,
+      b: 2
+    })).toEqual("$");
+
+    expect(expand("$$$", {
+      a: 1,
+      b: 2
+    })).toEqual("$$");
+
+    expect(expand("$$$a", {
+      a: 1,
+      b: 2
+    })).toEqual("$1");
+
+    expect(expand("$a$$", {
+      a: 1,
+      b: 2
+    })).toEqual("1$");
+  });
+});
